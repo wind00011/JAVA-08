9f8bf31de7d4edac7c88db5f2d7dab10ae5387aa
wind00011 <113489040+wind00011@users.noreply.github.com>
2022-10-08 23:56:40 +0800

    Add files via upload

---
+++ Class06.java
@@ -0,0 +1,21 @@
+package Java08;
+
+class CCircle {
+	double pi = 3.14;
+	double radius;
+
+	void show_periphery() {
+		System.out.println("periphery=" + 2 * pi * this.radius);
+	}
+}
+
+public class Class06 {
+
+	public static void main(String args[]) {
+		CCircle cir1 = new CCircle();
+		cir1.radius = 3.0;
+		cir1.show_periphery();
+
+	}
+
+}
